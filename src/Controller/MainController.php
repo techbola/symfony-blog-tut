203d467264e5f4faed2be66d28fc7a67b3d0a365
techbola <bfagbowore@gmail.com>
2019-09-10 15:08:37 +0100

    post controller and post entity created

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -13,6 +14,19 @@ class MainController extends AbstractController
     public function index()
     {
         return $this->render('main/index.html.twig');
+    }
 
+    /**
+     * @Route("/custom/{name?}", name="custom")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function custom(Request $request)
+    {
+        $name = $request->get('name');
+
+        return $this->render('main/show.html.twig', [
+            'name' => $name
+        ]);
     }
 }
